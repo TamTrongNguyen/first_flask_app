7902a8dfdc7a3d8d24cc331de20717344dab597e
Tam Nguyen <Tam.T.Nguyen1337@gmail.com>
2019-11-18 23:05:50 +0000

    changed heading and message

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 @app.route('/', methods=['GET'])
 def main():
     hostname = request.host_url
-    message = 'Thanks for visiting!'
+    message = 'How nice of you to stop by.'
     return render_template('main.html', host=hostname, msg=message)
 
 @app.route('/about', methods=['GET'])
